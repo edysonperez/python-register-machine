bab6d8477387974b348cfd0c9903bd69f2e142ff
Edyson <edysonperez@gmail.com>
2015-06-30 09:25:10 -0600

    Change the name of variables

--- UNIMACHINE.py
+++ UNIMACHINE.py
@@ -13,6 +13,12 @@ class test_REGISTER_MACHINE(unittest.TestCase):
 	def	test_VALID_NUMBER(self):
 		self.assertEqual(application.VALID_NUMBER(0),"INVALID_RANGE")
 		self.assertEqual(application.VALID_NUMBER(4),"INVALID_RANGE")
+	
+	def test_CHOISE_ENTER_PRODUCTS(self):
+		self.assertNotEqual(application.ENTER_PRODUCTS_YES_OR_NOT("y"),"ENTER_PRODUCTS") 
+		self.assertNotEqual(application.ENTER_PRODUCTS_YES_OR_NOT("n"),"BACK_MENU")
+		self.assertNotEqual(application.ENTER_PRODUCTS_YES_OR_NOT("STRING"),"INVALID_CHOISE")
+		self.assertNotEqual(application.ENTER_PRODUCTS_YES_OR_NOT(1),"INVALID_INPUT")
 
 if __name__ == "__main__":
 	unittest.main()
--- application.py
+++ application.py
@@ -1,34 +1,34 @@
 import os
 import sys
 
-def MENU_PRI():
+def EXIT():
+	os.system("clear")
+	print "        \n\n****THANK YOU FOR USING REGISTER MACHINE 3.0****\n\n"
+	sys.exit()
+
+def CLEAN_SCREEN():	
+	os.system("clear")
+
+def MAIN_MENU():
 	print "1. -Add an item"
 	print "2. -Sell Articles"
-	print "3. -Exit"	
-	ENTER = RAN()
+	print "3. -Exit"
+	ENTER = VERIFY_VALID_CHOISE()
 	CUTING(ENTER)
 
-def RAN():
+def VERIFY_VALID_CHOISE():
 	RANGE = True
 	while RANGE == True:
 		ENTER = VALID_CHOISE()
-		ENTER = ENTER_INT(ENTER)
+		ENTER = VERIFY_INTERGER(ENTER)
 		RANGE = VERIFY(ENTER)
 	return ENTER
-
-def CUTING(ENTER):
-	RANGE = True
-	while RANGE == True:
-		MSG = VALID_NUMBER(ENTER)
-		RANGE = CUT(MSG)
-		if RANGE == True:
-			ENTER = RAN()
 
 def VALID_CHOISE():
 	ENTER = raw_input("Enter number from 1 to 3 please\n>")
 	return ENTER
 
-def ENTER_INT(ENTER):
+def VERIFY_INTERGER(ENTER):
 	try:
 		ENTER = int(ENTER)
 		return ENTER
@@ -42,6 +42,14 @@ def VERIFY(ENTER):
 		RANGE = True
 	return RANGE
 
+def CUTING(ENTER):
+	RANGE = True
+	while RANGE == True:
+		MSG = VALID_NUMBER(ENTER)
+		RANGE = CUT(MSG)
+		if RANGE == True:
+			ENTER = VERIFY_VALID_CHOISE()
+
 def VALID_NUMBER(ENTER):
 	if ENTER < 1 or ENTER > 3:
 		MSG = "INVALID_RANGE"
@@ -52,16 +60,12 @@ def VALID_NUMBER(ENTER):
 
 def CORRECT_NUMBER(ENTER):
 	if ENTER == 1:
-		print "Selecciono la opcion 1"
 		return "OPTION_ONE"
-		
 	elif ENTER == 2:
-		print "Selecciono la opcion 2"
 		return "OPTION_TWO" 	
 	elif ENTER == 3:
 		EXIT()
 		return "OPTION_THREE"
-	
 
 def CUT(MSG):
 	if MSG == "OPTION_ONE" or MSG == "OPTION_TWO" or MSG == "OPTION_THREE":
@@ -70,12 +74,9 @@ def CUT(MSG):
 		RANGE = True
 	return RANGE
 
-def CLEAN_SCREEN():	
-	os.system("clear")
-	
-def EXIT():
-	os.system("clear")
-	print "        \n\n****THANK YOU FOR USING REGISTER MACHINE 3.0****\n\n"
-	sys.exit()
 if __name__ == "__main__":
-	MENU_PRI()
+	MAIN_MENU()
+
+
+
+
